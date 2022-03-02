dfa0c504db1cab8eba9e802f1fea5bad38daf33c
tomislav.cipric@q.agency <tomislav.cipric@q.agency>
2022-03-02 10:43:59 +0100

    changing api host

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://truckindigital.docker/api";
+const API_URL = "https://dev.laravel08.truckindigital.com/api";
 
 class AuthService {
   login(email, password) {
